80b05a3d6ec2ccbf554657c8fa42a1f8b123445d
J-Azuma <azumanotdetail+2@gmail.com>
2019-03-07 23:44:24 +0900

    add_linebot_controller

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -29,9 +29,9 @@ class LinebotController < ApplicationController
     private 
     def client 
         @client ||= Line::Bot::Client.new{|config|
-          config.channel_secret = ENV["LINE_CHENNEL_SECRET"]
-          config.channel_token = ENV["LINE_CHENNEL_TOKEN"]
-        }
+          config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+          config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+       }
     end
-    
+
 end
